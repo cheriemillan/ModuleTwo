d4c07f2edce7e1e6e7911b970d849b4b283e4ad5
CherieMillan <cherie99millan@gmail.com>
2024-07-21 22:57:51 -0400

    Added CalculateAverageGrade

--- ModuleTwo/Student.cs
+++ ModuleTwo/Student.cs
@@ -1,27 +1,45 @@
-﻿namespace ModuleTwo;
+﻿using System.Collections.Generic;
+using System;
+namespace ModuleTwo;
 
 public class Student
 {
     public string Name { get; set; }
     public int ID { get; set; }
-    public int Grades { get; set; }
+    public List<double> Grades { get; set; }
 
     public Student()
     {
         Name = "Bob";
         ID = 6352731;
-        Grades = 98;
+        Grades = new List<double>();
+        
     }
 
     public void AddGrade(double grades)
     {
-        
+       Grades.Add(grades);
     }
 
     public void AddGrade(params double[] grades)
     {
-        List<double> Grades = new List<double>();
         Grades.AddRange(grades);
+    }
+
+    public double CalculateAverageGrade()
+    {
+        if (Grades.Count == 0)
+        {
+            return 0;
+        }
+
+        double sum = 0;
+            for (int i = 0; i < Grades.Count; i++)
+            {
+                sum += Grades[i];
+
+            }
+        return sum / Grades.Count;
     }
 
 }
