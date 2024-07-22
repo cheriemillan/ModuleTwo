b84ba9256cea0ce79ca519b05e5c89d85142567c
CherieMillan <cherie99millan@gmail.com>
2024-07-21 22:08:10 -0400

    made a student class and made two add grade methods

---
+++ ModuleTwo/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace ModuleTwo;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public int Grades { get; set; }
+
+    public Student()
+    {
+        Name = "Bob";
+        ID = 6352731;
+        Grades = 98;
+    }
+
+    public void AddGrade(double grades)
+    {
+        
+    }
+
+    public void AddGrade(params double[] grades)
+    {
+        List<double> Grades = new List<double>();
+        Grades.AddRange(grades);
+    }
+
+}
